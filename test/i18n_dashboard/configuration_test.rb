bd3d4a68b0e43d7fdf40b024b4fc0b481b96f747
José Galisteo <ceritium@gmail.com>
2013-05-21 13:29:54 +0200

    fixing var name

--- test/i18n_dashboard/configuration_test.rb
+++ test/i18n_dashboard/configuration_test.rb
@@ -5,19 +5,19 @@ module I18nDashboard
   class ConfigurationTest < ActiveSupport::TestCase
 
     def setup
-      @blog_configuration = I18nDashboard::Configuration.new
+      @i18n_configuration = I18nDashboard::Configuration.new
     end
 
     test "should set :authentication_method to nil" do
-      assert_nil @blog_configuration.authentication_method
+      assert_nil @i18n_configuration.authentication_method
     end
 
     test "should set :layout to nil" do
-      assert_nil @blog_configuration.layout
+      assert_nil @i18n_configuration.layout
     end
 
     test "should set :inline_main_app_named_routes to false" do
-      assert !@blog_configuration.inline_main_app_named_routes
+      assert !@i18n_configuration.inline_main_app_named_routes
     end
   end
 end
