c6cbed4c30ffef5e146bba5f50793dadf71298d0
José Galisteo <ceritium@gmail.com>
2013-05-23 07:39:16 +0200

    gemfile rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 I18nDashboard::Engine.routes.draw do
 
-  match "/" => "translations#index", as: :i18n_dashboard_root
+  get "/" => "translations#index", as: :i18n_dashboard_root
   resources :translations, only: [:index, :create, :destroy]
 
 end
--- test/functional/i18n_dashboard/translations_controller_test.rb
+++ test/functional/i18n_dashboard/translations_controller_test.rb
@@ -3,31 +3,31 @@ require 'test_helper'
 module I18nDashboard
   class TranslationsControllerTest < ActionController::TestCase
 
-    test "should process index" do
-      process :index, use_route: :i18n_dashboard
+    test "should get index" do
+      get :index, use_route: :i18n_dashboard
       assert_response :success
       assert_not_nil assigns(:translations)
     end
 
-    test "should process create as HTML" do
-      process :create, key: 'test.lol', value: 'lol', locale: 'es', use_route: :i18n_dashboard, format: :html
+    test "should post create as HTML" do
+      post :create, key: 'test.lol', value: 'lol', locale: 'es', use_route: :i18n_dashboard, format: :html
       assert_redirected_to i18n_dashboard_root_path
     end
 
-    test "should process create as JS" do
-      process :create, key: 'test.lol', value: 'lol', locale: 'es', use_route: :i18n_dashboard, format: :js
+    test "should post create as JS" do
+      post :create, key: 'test.lol', value: 'lol', locale: 'es', use_route: :i18n_dashboard, format: :js
       assert_not_nil assigns(:digest)
       assert_response :success
     end
 
-    test "should process destroy as HTML" do
-      process :destroy, use_route: :i18n_dashboard, format: :html
+    test "should delete destroy as HTML" do
+      delete :destroy, use_route: :i18n_dashboard, format: :html
       assert_redirected_to i18n_dashboard_root_path
 
     end
 
-    test "should process destroy as JS" do
-      process :destroy, use_route: :i18n_dashboard, format: :js
+    test "should delete destroy as JS" do
+      delete :destroy, use_route: :i18n_dashboard, format: :js
       assert_response :success
     end
 
